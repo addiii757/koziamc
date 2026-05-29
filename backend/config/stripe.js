7f8b5aee2273a5b09cdb8bb07ffed221efa3c51d
addiii757 <adulek22032011@gmail.com>
2026-05-29 05:45:18 +0200

    Delete backend directory

--- backend/config/stripe.js
+++
@@ -1,9 +0,0 @@
-import Stripe from 'stripe'
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-})
-
-export const createCheckoutSession = async (params) => {
-  return stripe.checkout.sessions.create(params)
-}
